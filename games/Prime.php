0ec2c7852a0540d037f30a626e4396856fde39e6
Alexey Kholodov <alexeyhol@ya.ru>
2019-11-06 20:37:46 +0700

    Update Prime.php and brain-prime

--- games/Prime.php
+++ games/Prime.php
@@ -19,7 +19,42 @@ function hello()
 function prime()
 {
     global $name;
-    $question = mt_rand(1, 50);
-    line("\nQuestion: $question");
-    $answer = prompt("Your answer");
+    global $flag;
+    $check = 0;
+    $counter = 0;
+    $result = false;
+
+    while ($counter < 3) {
+        $question = mt_rand(1, 50);
+
+        for ($i = 1; $i <= $question; $i++) {
+            if ($question % $i == 0) {
+                $check += 1;
+            }
+        }
+    
+        if ($check == 2) {
+            $result = true;
+        }
+
+        line("\nQuestion: $question");
+        $answer = prompt("Your answer");
+
+        if ($answer == 'yes' && $result == true) {
+            line('Correct!');
+            $counter++;
+            $flag++;
+        } elseif ($answer == 'no' && $result == false) {
+            line('Correct!');
+            $counter++;
+            $flag++;
+        } elseif ($answer == 'yes' && $result == false) {
+            line("'{$answer}' is wrong answer ;(. Correct answer was 'no'.
+          Let's try again, {$name}!");
+            exit();
+        } elseif ($answer == 'no' && $result == true) {
+            line("'{$answer}' is wrong answer ;(. Correct answer was 'yes'.
+          Let's try again, {$name}!");
+        }
+    }
 }
