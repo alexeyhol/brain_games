dca4f31d67fa0b633b2d829251b60717c4deaa7e
Alexey Kholdov <alexeyhol@yandex.ru>
2020-01-19 20:28:06 +0700

    Create game engine function

--- games/calc.php
+++ games/calc.php
@@ -4,45 +4,37 @@ namespace BrainGames\Calc;
 
 use function cli\line;
 use function cli\prompt;
-use function Src\GameEngine\Hello;
-use function Src\GameEngine\Result;
+//use function Src\GameEngine\Hello;
+//use function Src\GameEngine\Result;
+//use function Src\GameEngine\Action;
+use function Src\GameEngine\Engine;
 
 function calc()
 {
-    global $name;
-    global $point;
+    $gameConditions = "What is the result of the expression?";
 
-    hello("What is the result of the expression?");
+    $questionAnswer = [];
 
     for ($i = 0; $i < 3; $i++) {
         $operators = ["+","-","*"];
         $operatorsKey = array_rand($operators, 1);
         $randomNumber1 = mt_rand(1, 50);
         $randomNumber2 = mt_rand(1, 50);
-        $operationLine = $randomNumber1 . ' ' . $operators[$operatorsKey]  . ' ' . $randomNumber2;
-
+        $question = $randomNumber1 . ' ' . $operators[$operatorsKey]  . ' ' . $randomNumber2;
         switch ($operatorsKey) {
             case 0:
-                $operation = (int)$randomNumber1 + (int)$randomNumber2;
+                $calc = (int)$randomNumber1 + (int)$randomNumber2;
                 break;
             case 1:
-                $operation = (int)$randomNumber1 - (int)$randomNumber2;
+                $calc = (int)$randomNumber1 - (int)$randomNumber2;
                 break;
             case 2:
-                $operation = (int)$randomNumber1 * (int)$randomNumber2;
+                $calc = (int)$randomNumber1 * (int)$randomNumber2;
                 break;
         }
 
-        line("\nQuestion: $operationLine");
-        $answer = prompt("Your answer");
-        if ($answer == $operation) {
-            line("Correct!");
-            $point++;
-        } else {
-            line("'{$answer}' is wrong answer ;(. Correct answer was '{$operation}'.
-            Let's try again, {$name}!");
-            exit();
-        }
-    }
-    result();
+        $questionAnswer['quest' . $i] = $question;
+        $questionAnswer['calc' . $i] = $calc;
+   }
+    engine($gameConditions, $questionAnswer);
 }
--- src/gameEngine.php
+++ src/gameEngine.php
@@ -7,7 +7,7 @@ use function cli\prompt;
 
 $name = '';
 $point = 0;
-
+/*
 function hello($gameConditions)
 {
     global $name;
@@ -18,11 +18,73 @@ function hello($gameConditions)
     line("Hello, %s!", $name);
 }
 
+function action($question, $correctAnswer)
+{
+    global $name;
+    global $point;
+    
+    for ($i = 0; $i < 3; $i++) {
+
+        line("\nQuestion: $question");
+        $answer = prompt("Your answer");
+        if ($answer == $correctAnswer) {
+            line("Correct!");
+            $point++;
+        } else {
+            line(
+                "'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.
+            Let's try again, {$name}!"
+            );
+            exit();
+        }
+    }
+}
+
 function result()
 {
     global $point;
     global $name;
     
+    if ($point == 3) {
+        line(' ');
+        line("Congratulations, {$name}!");
+    }
+}
+*/
+
+function engine(string $gameConditions, array $questionAnswer)
+{
+    global $name;
+    global $point;
+
+    /* User greeting*/
+
+    line("Welcome to Brain Games!");
+    line(' ');
+    line($gameConditions);
+    $name = prompt("May I have your name?");
+    line("Hello, %s!", $name);
+
+    /* Playing rounds */
+
+    for ($i = 0; $i < 3; $i++) {
+
+        line("\nQuestion: {$questionAnswer['quest' . $i]}");
+        $answer = prompt("Your answer");
+        if ($answer == $questionAnswer['calc' . $i]) {
+            line("Correct!");
+            $point++;
+        } else {
+            line(
+                "'{$answer}' is wrong answer ;(. Correct answer was '{$questionAnswer['calc' . $i]}'.
+            Let's try again, {$name}!"
+            );
+            exit();
+        }
+    }
+
+    /* Scoring, printing results */
+
     if ($point == 3) {
         line(' ');
         line("Congratulations, {$name}!");
